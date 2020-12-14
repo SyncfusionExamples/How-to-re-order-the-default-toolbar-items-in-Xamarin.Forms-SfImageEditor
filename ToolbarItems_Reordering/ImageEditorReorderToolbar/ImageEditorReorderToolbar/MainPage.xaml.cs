60364ae2a29fc374fc687c0dc22a514b5d0c430b
Hemalatha <hemalathavinai@gmail.com>
2020-12-14 13:18:00 +0530

    Sample included

---
+++ ToolbarItems_Reordering/ImageEditorReorderToolbar/ImageEditorReorderToolbar/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageEditorReorderToolbar
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var toolbarCollection = new ObservableCollection<Syncfusion.SfImageEditor.XForms.ToolbarItem>(editor.ToolbarSettings.ToolbarItems);
+            editor.ToolbarSettings.ToolbarItems.Clear();
+
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[2]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[4]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[0]);
+
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[3]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[1]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[8]);
+
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[6]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[7]);
+            editor.ToolbarSettings.ToolbarItems.Add(toolbarCollection[5]);
+
+        }
+    }
+}
+
---
+++ ToolbarItems_Reordering/ImageEditorReorderToolbar/ImageEditorReorderToolbar/ViewModel/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ImageEditorReorderToolbar
+{
+  public class ViewModel
+    {
+        public ImageSource Image { get; set; }
+
+        public ViewModel()
+        {
+            Image = ImageSource.FromResource("ImageEditorReorderToolbar.Image.jpeg");
+        }
+    }
+}
